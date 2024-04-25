1ade7598d2c1b9472fe53ada24472b3545800e8c
Mekentosh <m87022928044@gmail.com>
2024-04-26 01:31:59 +0500

    first commit

---
+++ FlutterMidka/lib/global_variables.dart
@@ -0,0 +1,34 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Mens Red Nike Shoes',
+    'price': 50.99,
+    'imageUrl': 'assets/images/shoes_1.png',
+    'company': 'Nike',
+    'sizes': [41, 42, 43, 44],
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Blue Shoes',
+    'price': 45.99,
+    'imageUrl': 'assets/images/shoes_2.png',
+    'company': 'Addidas',
+    'sizes': [39, 43, 45],
+  },
+  {
+    'id': '2',
+    'title': 'Red Running Shoes',
+    'price': 35.99,
+    'imageUrl': 'assets/images/shoes_3.png',
+    'company': 'Bata',
+    'sizes': [41, 42, 46],
+  },
+  {
+    'id': '3',
+    'title': 'Jordan Blue Shoes',
+    'price': 200.99,
+    'imageUrl': 'assets/images/shoes_4.png',
+    'company': 'Nike',
+    'sizes': [41, 44, 47],
+  },
+];
